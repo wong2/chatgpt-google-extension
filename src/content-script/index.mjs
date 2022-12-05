29cb4392d79de135a1ff01f3f89353b7d06aee19
ttttmr <32825326+ttttmr@users.noreply.github.com>
2022-12-05 15:57:46 +0800

    feat: support startpage and duckduckgo

--- src/content-script/index.mjs
+++ src/content-script/index.mjs
@@ -1,17 +1,10 @@
 import Browser from "webextension-polyfill";
 
+const container = document.createElement("div");
+
 async function run(question) {
-  const container = document.createElement("div");
   container.className = "chat-gpt-container";
   container.innerHTML = '<p class="loading">Waiting for ChatGPT response...</p>';
-
-  const siderbarContainer = document.getElementById("rhs");
-  if (siderbarContainer) {
-    siderbarContainer.prepend(container);
-  } else {
-    container.classList.add("sidebar-free");
-    document.getElementById("rcnt").appendChild(container);
-  }
 
   const port = Browser.runtime.connect();
   port.onMessage.addListener(function (msg) {
@@ -28,11 +21,40 @@ async function run(question) {
   port.postMessage({ question });
 }
 
-const searchInput = document.getElementsByName("q")[0];
-if (searchInput && searchInput.value) {
-  // only run on first page
-  const startParam = new URL(location.href).searchParams.get("start") || "0";
-  if (startParam === "0") {
+if(location.hostname.match("startpage")){
+  // startpage
+  document.querySelector(".layout-web__sidebar.layout-web__sidebar--web").appendChild(container);
+  const searchInput = document.getElementById("q");
+  if (searchInput && searchInput.value) {
     run(searchInput.value);
   }
+}else if(location.hostname.match("duckduckgo")){
+  // duckduckgo
+  document.getElementById("web_content_wrapper").prepend(container);
+  const searchInput = document.getElementsByName("q")[0];
+  if (searchInput && searchInput.value) {
+    // only run on first page
+    const startParam = new URL(location.href).searchParams.get("start") || "0";
+    if (startParam === "0") {
+      run(searchInput.value);
+    }
+  }
+}else if(location.hostname.match("google")){
+  // google
+  const siderbarContainer = document.getElementById("rhs");
+  if (siderbarContainer) {
+    siderbarContainer.prepend(container);
+  } else {
+    container.classList.add("sidebar-free");
+    document.getElementById("rcnt").appendChild(container);
+  }
+  const searchInput = document.getElementsByName("q")[0];
+  if (searchInput && searchInput.value) {
+    // only run on first page
+    const startParam = new URL(location.href).searchParams.get("start") || "0";
+    if (startParam === "0") {
+      run(searchInput.value);
+    }
+  }
 }
+
