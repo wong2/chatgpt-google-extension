405653abe4f69033c9918b46e39eef3646fc0a75
Wang Dàpéng <wonderfuly@gmail.com>
2022-12-06 23:27:20 +0800

    Add markdown rendering (#42)
    * feature: add markdown to html support
    
    * feat: add markdown render, code highlight and code copy button
    
    * Simplify and fix styles
    
    Co-authored-by: yueqing-wolai <79960383+yueqing-wolai@users.noreply.github.com>

--- src/content-script/index.mjs
+++ src/content-script/index.mjs
@@ -1,6 +1,9 @@
+import MarkdownIt from "markdown-it";
 import Browser from "webextension-polyfill";
 
 async function run(question) {
+  const markdown = new MarkdownIt();
+
   const container = document.createElement("div");
   container.className = "chat-gpt-container";
   container.innerHTML = '<p class="loading">Waiting for ChatGPT response...</p>';
@@ -16,8 +19,9 @@ async function run(question) {
   const port = Browser.runtime.connect();
   port.onMessage.addListener(function (msg) {
     if (msg.answer) {
-      container.innerHTML = '<p><span class="prefix">ChatGPT:</span><pre></pre></p>';
-      container.querySelector("pre").textContent = msg.answer;
+      container.innerHTML =
+        '<p class="prefix">ChatGPT:</p><div id="answer" class="markdown-body"></div>';
+      container.querySelector("#answer").innerHTML = markdown.render(msg.answer);
     } else if (msg.error === "UNAUTHORIZED") {
       container.innerHTML =
         '<p>Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</p>';
