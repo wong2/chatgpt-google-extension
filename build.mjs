2753fffd979034d019481ce0e8aa1a4be31eec30
wong2 <wonderfuly@gmail.com>
2022-12-07 16:44:15 +0800

    Fix build script

--- build.mjs
+++ build.mjs
@@ -1,32 +1,29 @@
-import esbuild from 'esbuild'
 import archiver from 'archiver'
+import esbuild from 'esbuild'
 import fs from 'fs'
 
 const outdir = 'build'
 
 async function deleteOldDir() {
-  fs.rmSync(outdir, {recursive: true, force: true})
+  fs.rmSync(outdir, { recursive: true, force: true })
 }
 
 async function runEsbuild() {
   await esbuild.build({
-    entryPoints: [
-      'src/content-script/index.mjs',
-      'src/background/index.mjs'
-    ],
+    entryPoints: ['src/content-script/index.mjs', 'src/background/index.mjs'],
     bundle: true,
-    outdir: outdir
+    outdir: outdir,
   })
 }
 
 async function zipFiles(entryPoints, outfile) {
   const output = fs.createWriteStream(outfile)
   const archive = archiver('zip', {
-    zlib: {level: 9}
+    zlib: { level: 9 },
   })
   archive.pipe(output)
   for (const entryPoint of entryPoints) {
-    archive.file(entryPoint.src, {name: entryPoint.dst})
+    archive.file(entryPoint.src, { name: entryPoint.dst })
   }
   await archive.finalize()
 }
@@ -35,25 +32,25 @@ async function build() {
   await deleteOldDir()
   await runEsbuild()
 
+  const commonFiles = [
+    { src: 'build/content-script/index.js', dst: 'content-script/index.js' },
+    { src: 'build/background/index.js', dst: 'background/index.js' },
+    { src: 'src/github-markdown.css', dst: 'github-markdown.css' },
+    { src: 'src/styles.css', dst: 'styles.css' },
+    { src: 'src/logo.png', dst: 'logo.png' },
+  ]
+
   // chromium
-  await zipFiles([
-      {src: 'build/content-script/index.js', dst: 'content-script/index.js'},
-      {src: 'build/background/index.js', dst: 'background/index.js'},
-      {src: 'src/manifest.json', dst: 'manifest.json'},
-      {src: 'src/styles.css', dst: 'styles.css'},
-      {src: 'src/logo.png', dst: 'logo.png'}
-    ],
-    `./${outdir}/chromium.zip`)
+  await zipFiles(
+    [...commonFiles, { src: 'src/manifest.json', dst: 'manifest.json' }],
+    `./${outdir}/chromium.zip`,
+  )
 
   // firefox
-  await zipFiles([
-      {src: 'build/content-script/index.js', dst: 'content-script/index.js'},
-      {src: 'build/background/index.js', dst: 'background/index.js'},
-      {src: 'src/manifest.v2.json', dst: 'manifest.json'},
-      {src: 'src/styles.css', dst: 'styles.css'},
-      {src: 'src/logo.png', dst: 'logo.png'}
-    ],
-    `./${outdir}/firefox.zip`)
+  await zipFiles(
+    [...commonFiles, { src: 'src/manifest.v2.json', dst: 'manifest.json' }],
+    `./${outdir}/firefox.zip`,
+  )
 }
 
 build()
