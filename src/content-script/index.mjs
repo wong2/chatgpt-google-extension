c4e4708eac006d650273802bed3995d696cd55eb
Jos Storer <joshua1466587594@outlook.com>
2022-12-07 15:39:30 +0800

    update code style for more clear PR

--- src/content-script/index.mjs
+++ src/content-script/index.mjs
@@ -1,67 +1,68 @@
-import Browser from 'webextension-polyfill'
-import {getPossibleElementByClassArray, getPossibleElementByIdArray, getPossibleElementByNameArray} from './utils.mjs'
+import Browser from "webextension-polyfill";
+import {getPossibleElementByClassArray, getPossibleElementByIdArray, getPossibleElementByNameArray} from "./utils.mjs"
 
 const config = {
   // input element name of search box
-  inputName: ['q', 'wd', 'text', 'query', 'p'],
+  inputName: ["q", "wd", "text", "query", "p"],
   // prepend child to
   sidebarContainerId: [
-    'rhs', 'content_right', 'b_rrsr', 'search-result-aside', 'b_context', 'right', 'sub_pack', 'sidebar_results',
-    'side-right'
+    "rhs", "content_right", "b_rrsr", "search-result-aside", "b_context", "right", "sub_pack", "sidebar_results",
+    "side-right"
   ],
   // if above ids not exist, prepend child to
   sidebarContainerClass: [
-    'right-content-box _0_right_sidebar', 'results--sidebar js-results-sidebar', 'layout-web__sidebar layout-web__sidebar--web',
-    'Contents__inner Contents__inner--sub', 'sidebar web__sidebar'
+    "right-content-box _0_right_sidebar", "results--sidebar js-results-sidebar", "layout-web__sidebar layout-web__sidebar--web",
+    "Contents__inner Contents__inner--sub", "sidebar web__sidebar"
   ],
   // if above all not exist, append child to
   appendContainerId: [
-    'rcnt', '_0_app_content', 'content__left', 'container', 'content', 'main', 'contents__wrap', 'cols', 'links_wrapper', 'wrapper'
+    "rcnt", "_0_app_content", "content__left", "container", "content", "main", "contents__wrap", "cols", "links_wrapper", "wrapper"
   ],
   // if above all not exist, append child to
   appendContainerClass: [
-    'content__left', 'layout-web__body layout-web__body--desktop'
+    "content__left", "layout-web__body layout-web__body--desktop"
   ]
 }
 
 async function run(question) {
-  const container = document.createElement('div')
-  container.className = 'chat-gpt-container'
-  container.innerHTML = '<p class="loading">Waiting for ChatGPT response...</p>'
+  const container = document.createElement("div");
+  container.className = "chat-gpt-container";
+  container.innerHTML = '<p class="loading">Waiting for ChatGPT response...</p>';
 
   const siderbarContainer =
     getPossibleElementByIdArray(config.sidebarContainerId)
-    || getPossibleElementByClassArray(config.sidebarContainerClass)
+    || getPossibleElementByClassArray(config.sidebarContainerClass);
   if (siderbarContainer) {
-    siderbarContainer.prepend(container)
+    siderbarContainer.prepend(container);
   } else {
-    container.classList.add('sidebar-free')
+    container.classList.add("sidebar-free");
     const appendContainer =
       getPossibleElementByIdArray(config.appendContainerId)
-      || getPossibleElementByClassArray(config.appendContainerClass)
+      || getPossibleElementByClassArray(config.appendContainerClass);
     if (appendContainer)
-      appendContainer.appendChild(container)
+      appendContainer.appendChild(container);
   }
-  const port = Browser.runtime.connect()
+
+  const port = Browser.runtime.connect();
   port.onMessage.addListener(function (msg) {
     if (msg.answer) {
-      container.innerHTML = '<p><span class="prefix">ChatGPT:</span><pre></pre></p>'
-      container.querySelector('pre').textContent = msg.answer
-    } else if (msg.error === 'UNAUTHORIZED') {
+      container.innerHTML = '<p><span class="prefix">ChatGPT:</span><pre></pre></p>';
+      container.querySelector("pre").textContent = msg.answer;
+    } else if (msg.error === "UNAUTHORIZED") {
       container.innerHTML =
-        '<p>Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</p>'
+        '<p>Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</p>';
     } else {
-      container.innerHTML = '<p>Failed to load response from ChatGPT</p>'
+      container.innerHTML = "<p>Failed to load response from ChatGPT</p>";
     }
-  })
-  port.postMessage({question})
+  });
+  port.postMessage({ question });
 }
 
-const searchInput = getPossibleElementByNameArray(config.inputName)
+const searchInput = getPossibleElementByNameArray(config.inputName);
 if (searchInput && searchInput.value) {
   // only run on first page
-  const startParam = new URL(location.href).searchParams.get('start') || '0'
-  if (startParam === '0') {
-    run(searchInput.value)
+  const startParam = new URL(location.href).searchParams.get("start") || "0";
+  if (startParam === "0") {
+    run(searchInput.value);
   }
 }
--- src/content-script/utils.mjs
+++ src/content-script/utils.mjs
@@ -1,35 +1,35 @@
 export function getPossibleElementByNameArray(elementNameArray) {
-  let element
+  let element;
   elementNameArray.some(
     (e) => {
-      element = document.getElementsByName(e)[0]
+      element = document.getElementsByName(e)[0];
       if (element)
-        return true
+        return true;
     }
-  )
-  return element
+  );
+  return element;
 }
 
 export function getPossibleElementByIdArray(elementIdArray) {
-  let element
+  let element;
   elementIdArray.some(
     (e) => {
-      element = document.getElementById(e)
+      element = document.getElementById(e);
       if (element)
-        return true
+        return true;
     }
-  )
-  return element
+  );
+  return element;
 }
 
 export function getPossibleElementByClassArray(elementClassArray) {
-  let element
+  let element;
   elementClassArray.some(
     (e) => {
-      element = document.getElementsByClassName(e)[0]
+      element = document.getElementsByClassName(e)[0];
       if (element)
-        return true
+        return true;
     }
-  )
-  return element
+  );
+  return element;
 }
