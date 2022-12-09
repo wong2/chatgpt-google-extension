49d3c1bbf5d2d4a0448d45266dcaf67b710d1db1
wong2 <wonderfuly@gmail.com>
2022-12-09 20:15:10 +0800

    Fix search engine compatible issues

--- src/content-script/index.mjs
+++ src/content-script/index.mjs
@@ -1,26 +1,26 @@
 import 'github-markdown-css'
 import MarkdownIt from 'markdown-it'
 import Browser from 'webextension-polyfill'
-import { getPossibleElementByQuerySelector } from './utils.mjs'
-import { config } from './engine-match-config.mjs'
+import { config } from './search-engine-configs.mjs'
 import './styles.css'
+import { getPossibleElementByQuerySelector } from './utils.mjs'
 
-async function run(question) {
+async function run(question, siteConfig) {
   const markdown = new MarkdownIt()
 
   const container = document.createElement('div')
   container.className = 'chat-gpt-container'
-  container.innerHTML = '<p class="loading">Waiting for ChatGPT response...</p>'
+  container.innerHTML = '<p class="gpt-loading">Waiting for ChatGPT response...</p>'
 
-  const siderbarContainer = getPossibleElementByQuerySelector(
-    config[siteName].sidebarContainerQuery,
-  )
+  const siderbarContainer = getPossibleElementByQuerySelector(siteConfig.sidebarContainerQuery)
   if (siderbarContainer) {
     siderbarContainer.prepend(container)
   } else {
     container.classList.add('sidebar-free')
-    const appendContainer = getPossibleElementByQuerySelector(config[siteName].appendContainerQuery)
-    if (appendContainer) appendContainer.appendChild(container)
+    const appendContainer = getPossibleElementByQuerySelector(siteConfig.appendContainerQuery)
+    if (appendContainer) {
+      appendContainer.appendChild(container)
+    }
   }
 
   const port = Browser.runtime.connect()
@@ -40,15 +40,14 @@ async function run(question) {
   port.postMessage({ question })
 }
 
-const matchedSites = Object.keys(config)
-const siteRegex = new RegExp(`(${matchedSites.join('|')})`)
-const siteName = document.location.hostname.match(siteRegex)[0]
+const siteRegex = new RegExp(Object.keys(config).join('|'))
+const siteName = location.hostname.match(siteRegex)[0]
 
 const searchInput = getPossibleElementByQuerySelector(config[siteName].inputQuery)
 if (searchInput && searchInput.value) {
   // only run on first page
   const startParam = new URL(location.href).searchParams.get('start') || '0'
   if (startParam === '0') {
-    run(searchInput.value)
+    run(searchInput.value, config[siteName])
   }
 }
