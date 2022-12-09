2a8599cd85071624e6c46092e484b772c3dd017c
wong2 <wonderfuly@gmail.com>
2022-12-09 19:51:40 +0800

    Format code with prettier

--- src/content-script/engine-match-config.mjs
+++ src/content-script/engine-match-config.mjs
@@ -10,62 +10,62 @@
 export const config = {
   google: {
     inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: ["#rhs"],
-    appendContainerQuery: ["#rcnt"]
+    sidebarContainerQuery: ['#rhs'],
+    appendContainerQuery: ['#rcnt'],
   },
   bing: {
     inputQuery: ["textarea[name='q']"],
-    sidebarContainerQuery: ["#b_context"],
-    appendContainerQuery: []
+    sidebarContainerQuery: ['#b_context'],
+    appendContainerQuery: [],
   },
   yahoo: {
     inputQuery: ["input[name='p']"],
-    sidebarContainerQuery: ["#right", ".Contents__inner.Contents__inner--sub"],
-    appendContainerQuery: ["#cols", "#contents__wrap"]
+    sidebarContainerQuery: ['#right', '.Contents__inner.Contents__inner--sub'],
+    appendContainerQuery: ['#cols', '#contents__wrap'],
   },
   duckduckgo: {
     inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: [".results--sidebar.js-results-sidebar"],
-    appendContainerQuery: ["#links_wrapper"]
+    sidebarContainerQuery: ['.results--sidebar.js-results-sidebar'],
+    appendContainerQuery: ['#links_wrapper'],
   },
   startpage: {
     inputQuery: ["input[name='query']"],
-    sidebarContainerQuery: [".layout-web__sidebar.layout-web__sidebar--web"],
-    appendContainerQuery: [".layout-web__body.layout-web__body--desktop"]
+    sidebarContainerQuery: ['.layout-web__sidebar.layout-web__sidebar--web'],
+    appendContainerQuery: ['.layout-web__body.layout-web__body--desktop'],
   },
   baidu: {
     inputQuery: ["input[name='wd']"],
-    sidebarContainerQuery: ["#content_right"],
-    appendContainerQuery: ["#container"]
+    sidebarContainerQuery: ['#content_right'],
+    appendContainerQuery: ['#container'],
   },
   kagi: {
     inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: [".right-content-box._0_right_sidebar"],
-    appendContainerQuery: ["#_0_app_content"],
+    sidebarContainerQuery: ['.right-content-box._0_right_sidebar'],
+    appendContainerQuery: ['#_0_app_content'],
   },
   yandex: {
     inputQuery: ["input[name='text']"],
-    sidebarContainerQuery: ["#search-result-aside"],
-    appendContainerQuery: []
+    sidebarContainerQuery: ['#search-result-aside'],
+    appendContainerQuery: [],
   },
   naver: {
     inputQuery: ["input[name='query']"],
-    sidebarContainerQuery: ["#sub_pack"],
-    appendContainerQuery: ["#content"]
+    sidebarContainerQuery: ['#sub_pack'],
+    appendContainerQuery: ['#content'],
   },
   brave: {
     inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: ["#side-right"],
-    appendContainerQuery: []
+    sidebarContainerQuery: ['#side-right'],
+    appendContainerQuery: [],
   },
   searx: {
     inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: ["#sidebar_results"],
-    appendContainerQuery: []
+    sidebarContainerQuery: ['#sidebar_results'],
+    appendContainerQuery: [],
   },
   ecosia: {
     inputQuery: ["input[name='q']"],
-    sidebarContainerQuery: [".sidebar.web__sidebar"],
-    appendContainerQuery: ["#main"]
-  }
+    sidebarContainerQuery: ['.sidebar.web__sidebar'],
+    appendContainerQuery: ['#main'],
+  },
 }
--- src/content-script/index.mjs
+++ src/content-script/index.mjs
@@ -1,50 +1,51 @@
-import MarkdownIt from "markdown-it";
-import Browser from "webextension-polyfill";
-import {getPossibleElementByQuerySelector} from "./utils.mjs"
-import {config} from "./engine-match-config.mjs"
+import MarkdownIt from 'markdown-it'
+import Browser from 'webextension-polyfill'
+import { getPossibleElementByQuerySelector } from './utils.mjs'
+import { config } from './engine-match-config.mjs'
 
 async function run(question) {
-  const markdown = new MarkdownIt();
+  const markdown = new MarkdownIt()
 
-  const container = document.createElement("div");
-  container.className = "chat-gpt-container";
-  container.innerHTML = '<p class="loading">Waiting for ChatGPT response...</p>';
+  const container = document.createElement('div')
+  container.className = 'chat-gpt-container'
+  container.innerHTML = '<p class="loading">Waiting for ChatGPT response...</p>'
 
-  const siderbarContainer = getPossibleElementByQuerySelector(config[siteName].sidebarContainerQuery);
+  const siderbarContainer = getPossibleElementByQuerySelector(
+    config[siteName].sidebarContainerQuery,
+  )
   if (siderbarContainer) {
-    siderbarContainer.prepend(container);
+    siderbarContainer.prepend(container)
   } else {
-    container.classList.add("sidebar-free");
-    const appendContainer = getPossibleElementByQuerySelector(config[siteName].appendContainerQuery);
-    if (appendContainer)
-      appendContainer.appendChild(container);
+    container.classList.add('sidebar-free')
+    const appendContainer = getPossibleElementByQuerySelector(config[siteName].appendContainerQuery)
+    if (appendContainer) appendContainer.appendChild(container)
   }
 
-  const port = Browser.runtime.connect();
+  const port = Browser.runtime.connect()
   port.onMessage.addListener(function (msg) {
     if (msg.answer) {
       container.innerHTML =
-        '<p class="prefix">ChatGPT:</p><div id="answer" class="markdown-body" dir="auto"></div>';
-      container.querySelector("#answer").innerHTML = markdown.render(msg.answer);
-    } else if (msg.error === "UNAUTHORIZED") {
+        '<p class="prefix">ChatGPT:</p><div id="answer" class="markdown-body" dir="auto"></div>'
+      container.querySelector('#answer').innerHTML = markdown.render(msg.answer)
+    } else if (msg.error === 'UNAUTHORIZED') {
       container.innerHTML =
-        '<p>Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</p>';
+        '<p>Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</p>'
     } else {
-      container.innerHTML = "<p>Failed to load response from ChatGPT</p>";
+      container.innerHTML = '<p>Failed to load response from ChatGPT</p>'
     }
-  });
-  port.postMessage({ question });
+  })
+  port.postMessage({ question })
 }
 
-const matchedSites = Object.keys(config);
-const siteRegex = new RegExp(`(${matchedSites.join('|')})`);
-const siteName = document.location.hostname.match(siteRegex)[0];
+const matchedSites = Object.keys(config)
+const siteRegex = new RegExp(`(${matchedSites.join('|')})`)
+const siteName = document.location.hostname.match(siteRegex)[0]
 
-const searchInput = getPossibleElementByQuerySelector(config[siteName].inputQuery);
+const searchInput = getPossibleElementByQuerySelector(config[siteName].inputQuery)
 if (searchInput && searchInput.value) {
   // only run on first page
-  const startParam = new URL(location.href).searchParams.get("start") || "0";
-  if (startParam === "0") {
-    run(searchInput.value);
+  const startParam = new URL(location.href).searchParams.get('start') || '0'
+  if (startParam === '0') {
+    run(searchInput.value)
   }
 }
