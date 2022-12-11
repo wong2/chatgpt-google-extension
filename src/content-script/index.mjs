06df8d7e82abf5e2abb05e95b0d65551824e4373
wong2 <wonderfuly@gmail.com>
2022-12-11 12:33:16 +0800

    Move markdown initialization code to module

--- src/content-script/index.mjs
+++ src/content-script/index.mjs
@@ -1,36 +1,13 @@
 import 'github-markdown-css'
-import MarkdownIt from 'markdown-it'
 import Browser from 'webextension-polyfill'
+import { getMarkdownRenderer } from './markdown.mjs'
 import { config } from './search-engine-configs.mjs'
 import './styles.css'
 import { getPossibleElementByQuerySelector } from './utils.mjs'
 
-const markdown = new MarkdownIt({
-  // Autoconvert URL-like text to links
-  linkify: true
-})
-
-// https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
-// Remember old renderer, if overridden, or proxy to default renderer
-const defaultRender = markdown.renderer.rules.link_open || function(tokens, idx, options, env, self) {
-  return self.renderToken(tokens, idx, options)
-}
-
-markdown.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-  // If you are sure other plugins can't add `target` - drop check below
-  const aIndex = tokens[idx].attrIndex('target')
-
-  if (aIndex < 0) {
-    tokens[idx].attrPush(['target', '_blank']) // add new attribute
-  } else {
-    tokens[idx].attrs[aIndex][1] = '_blank'    // replace value of existing attr
-  }
-
-  // pass token to default renderer.
-  return defaultRender(tokens, idx, options, env, self)
-}
-
 async function run(question, siteConfig) {
+  const markdown = getMarkdownRenderer()
+
   const container = document.createElement('div')
   container.className = 'chat-gpt-container'
   container.innerHTML = '<p class="gpt-loading">Waiting for ChatGPT response...</p>'
