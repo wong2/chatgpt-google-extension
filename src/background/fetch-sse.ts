65e81decbc4defea20e598041afa387d64c7d05b
wong2 <wonderfuly@gmail.com>
2022-12-30 15:02:39 +0800

    Improve api error message

--- src/background/fetch-sse.ts
+++ src/background/fetch-sse.ts
@@ -1,4 +1,5 @@
 import { createParser } from 'eventsource-parser'
+import { isEmpty } from 'lodash-es'
 import { streamAsyncIterable } from './stream-async-iterable.js'
 
 export async function fetchSSE(
@@ -8,8 +9,8 @@ export async function fetchSSE(
   const { onMessage, ...fetchOptions } = options
   const resp = await fetch(resource, fetchOptions)
   if (!resp.ok) {
-    const detail = (await resp.json().catch(() => ({}))).detail
-    throw new Error(detail || `${resp.status} ${resp.statusText}`)
+    const error = await resp.json().catch(() => ({}))
+    throw new Error(!isEmpty(error) ? JSON.stringify(error) : `${resp.status} ${resp.statusText}`)
   }
   const parser = createParser((event) => {
     if (event.type === 'event') {
