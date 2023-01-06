b7659223c39b77acf3115b0e91e9d7429d536bdb
wong2 <wonderfuly@gmail.com>
2023-01-06 19:20:19 +0800

    Add logo to popup

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,4 +1,5 @@
 import { GearIcon, GlobeIcon } from '@primer/octicons-react'
+import logo from '../logo.png'
 import { useCallback } from 'react'
 import useSWR from 'swr'
 import Browser from 'webextension-polyfill'
@@ -17,8 +18,10 @@ function App() {
 
   return (
     <div className="flex flex-col h-full">
-      <div className="mb-2 flex flex-row items-center justify-between px-1">
-        <p className="text-base font-semibold m-0">ChatGPT for Google</p>
+      <div className="mb-2 flex flex-row items-center px-1">
+        <img src={logo} className="w-5 h-5 rounded-sm" />
+        <p className="text-sm font-semibold m-0 ml-1">ChatGPT for Google</p>
+        <div className="grow"></div>
         <span className="cursor-pointer leading-[0]" onClick={openOptionsPage}>
           <GearIcon size={16} />
         </span>
