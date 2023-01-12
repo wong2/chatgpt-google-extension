9ebaa8a6736dd2c27ff5f6ba11bd3d898e3a9868
wong2 <wonderfuly@gmail.com>
2023-01-12 14:21:47 +0800

    Remove posthog

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,18 +1,3 @@
-import posthog from 'posthog-js'
-import { getExtensionVersion } from './utils'
-
-posthog.init('phc_vYOTewBdDTJImMRnKCUyymBkZbZmBM9kUHxHHCgTCrV', {
-  api_host: 'https://app.posthog.com',
-  persistence: 'localStorage',
-  autocapture: false,
-  capture_pageview: false,
-  disable_session_recording: true,
-  property_blacklist: ['$current_url', '$pathname'],
-  loaded: (pg) => {
-    pg.people.set({ ext_version: getExtensionVersion() })
-  },
-})
-
 export function captureEvent(event: string, properties?: object) {
-  posthog.capture(event, properties)
+  // TODO
 }
