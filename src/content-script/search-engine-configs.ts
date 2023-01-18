719df150b607a07cff52ad64e0e52a4d9e8e7580
smart-techno <mickael.kasinski@gmail.com>
2023-01-18 14:01:39 +0100

    add qwant.com in search engines

--- src/content-script/search-engine-configs.ts
+++ src/content-script/search-engine-configs.ts
@@ -72,4 +72,29 @@ export const config: Record<string, SearchEngine> = {
     sidebarContainerQuery: ['#sidebar_results'],
     appendContainerQuery: [],
   },
+  qwant: {
+    inputQuery: ["[name='q']"],
+    sidebarContainerQuery: ['.is-sidebar'],
+    appendContainerQuery: [],
+    watchRouteChange(callback) {
+      const targetNode = document.querySelector('#root')!
+      const observer = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) {
+          if (mutation.type === 'childList') {
+            mutation.addedNodes.forEach(function (node) {
+              if (
+                'className' in node &&
+                typeof node.className === 'string' &&
+                node.className.includes('is-sidebar')
+              ) {
+                callback()
+              }
+            })
+          }
+        }
+      })
+      const config = { attributes: true, childList: true, subtree: true }
+      observer.observe(targetNode, config)
+    },
+  },
 }
