73940f3d78dede936529b9573df25f40a850ceca
wong2 <wonderfuly@gmail.com>
2023-02-05 20:01:52 +0800

    Show extension version in options page

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -11,7 +11,7 @@ import {
   updateUserConfig,
 } from '../config'
 import logo from '../logo.png'
-import { detectSystemColorScheme } from '../utils'
+import { detectSystemColorScheme, getExtensionVersion } from '../utils'
 import ProviderSelect from './ProviderSelect'
 
 function OptionsPage(props: { theme: Theme; onThemeChange: (theme: Theme) => void }) {
@@ -57,7 +57,7 @@ function OptionsPage(props: { theme: Theme; onThemeChange: (theme: Theme) => voi
       <nav className="flex flex-row justify-between items-center mt-5 px-2">
         <div className="flex flex-row items-center gap-2">
           <img src={logo} className="w-10 h-10 rounded-lg" />
-          <span className="font-semibold">ChatGPT for Google</span>
+          <span className="font-semibold">ChatGPT for Google (v{getExtensionVersion()})</span>
         </div>
         <div className="flex flex-row gap-3">
           <a href="https://chatgpt-for-google.canny.io/changelog" target="_blank" rel="noreferrer">
