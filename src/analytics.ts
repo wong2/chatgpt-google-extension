3ce0a4d71bf71a64fca07248012373ac497eda72
wong2 <wonderfuly@gmail.com>
2023-02-06 21:55:27 +0800

    Remove axiom code

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,10 +1,3 @@
 export function captureEvent(event: string, properties?: object) {
-  fetch('https://api.axiom.co/v1/datasets/extension/ingest', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-ndjson',
-      Authorization: `Bearer ${process.env.AXIOM_TOKEN}`,
-    },
-    body: JSON.stringify({ event, ...(properties || {}) }),
-  }).catch((err) => console.error('captureEvent error', err))
+  // TODO
 }
